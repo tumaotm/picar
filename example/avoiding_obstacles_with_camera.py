826317945ac0abed736a4a7c902cd4b5bd276d62
Matthew Yin <yinzhiyuan86@gmail.com>
2022-06-01 21:46:23 -0400

    Init - create new file

---
+++ example/avoiding_obstacles_with_camera.py
@@ -0,0 +1,24 @@
+from picarx import Picarx
+
+
+def main():
+    try:
+        px = Picarx()
+        #px = Picarx(ultrasonic_pins=['D2','D3']) # tring, echo
+        px.forward(10)
+        while True:
+            distance = px.ultrasonic.read()
+            print("distance: ",distance)
+            if distance > 0 and distance < 300:
+                if distance < 25:
+                    px.backward(25)
+                    px.set_dir_servo_angle(-35)
+                else:
+                    px.set_dir_servo_angle(0)
+    finally:
+        px.stop()
+
+
+if __name__ == "__main__":
+    main()
+
